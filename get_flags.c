7f7c8c46febb36d03c50e730222d517599dd78a1
DamianJennifer <damianchinemerum@gmail.com>
2022-10-17 19:01:14 -0700

    kj printf

---
+++ get_flags.c
@@ -0,0 +1,34 @@
+#include <main.h>
+
+/**
+ * get_flag - function to turn all flags if _printf finds a flag modifier in
+ * the string format
+ * @s : character to hold the flag specifier
+ * @f : pointer to the struct flags
+ * Return : return 1 if flag has been turned on or 0 if otherwise
+ */
+
+int get_flag(char s, flags_t *f)
+{
+        int i = 0
+
+                switch (s)
+                {
+                        case '+':
+                        f -> plus = 1;
+                        i = 1;
+                        break;
+
+                        case ' ':
+                        f -> space = 1;
+                        i = 1;
+                        break;
+
+                        case '#':
+			f -> hash = 1:
+			i =1:
+			break;
+		}
+	return (i);
+
+}
