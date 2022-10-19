1f2b01ba5cc9bac4aad1ffc5f9473e86b857e5d3
attama <kevinejikez@gmail.com>
2022-10-18 17:16:51 -0700

    final file

--- print_bases.c
+++ print_bases.c
@@ -14,7 +14,7 @@ int print_hex(va_list l, flags_t *f)
 	char *str = convert(num, 16, 1);
 	int count = 0;
 
-	if (f -> hash == 1 && str[0] != '0')
+	if (f->hash == 1 && str[0] != '0')
 		count += _puts("0x");
                 count += _puts(str);
 		return (count);
@@ -34,7 +34,7 @@ int print_hex_big(va_list l, flags_t *f)
         char *str = convert(num, 16, 0);
         int count = 0;
 
-        if (f -> hash == 1 && str[0] != '0')
+        if (f->hash == 1 && str[0] != '0')
                 count += _puts("0X");
                 count += _puts(str);
                 return (count);
@@ -68,12 +68,8 @@ int print_octal(va_list l, flags_t *f)
         char *str = convert(num, 8, 0);
         int count = 0;
 
-        if (f -> hash == 1 && str[0] != '0')
+        if (f->hash == 1 && str[0] != '0')
                 count += _putchar('0');
                 count += _puts(str);
                 return (count);
 }
-
-
-
-
