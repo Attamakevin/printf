59b731acea8ddd7f854aa7cebfa1947c3ddb53c4
attama <kevinejikez@gmail.com>
2022-10-18 18:22:09 -0700

    hotfix

--- get_print.c
+++ get_print.c
@@ -6,32 +6,51 @@
  * @s : character that holds the conversion specifier
  * Return : return a pointer to the matching printing function
  */
- 
-int (*get_print(char s))(va_list, flags_t*)
+ int (*get_print(char s))(va_list, flags_t *)
+
 {
+
 	ph func_arr[] = {
+
 		{'i', print_int},
-		{'s', print_str},
+
+		{'s', print_string},
+
 		{'c', print_char},
+
 		{'d', print_int},
+
 		{'u', print_unsigned},
+
 		{'x', print_hex},
+
 		{'X', print_hex_big},
+
 		{'b', print_binary},
-		{'O', print_octal},
+
+		{'o', print_octal},
+
 		{'R', print_rot13},
+
 		{'r', print_rev},
-		{'S', print_bigs},
-		['p', print_address},
-	        {'%', print_percent}
+
+		{'S', print_bigS},
+
+		{'p', print_address},
+
+		{'%', print_percent}
+
 		};
 
+	int flags = 14;
 
-int flags = 14;
-register int i;
 
-for (i = 0; i < flags; i++)
-if (func_arr[i].c == s)
-	return(func_arr[i].f);
+	register int i;
+
+	for (i = 0; i < flags; i++)
+
+		if (func_arr[i].c == s)
+
+			return (func_arr[i].f);
 	return (NULL);
 }
